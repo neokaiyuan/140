447fcd8e769ed8604454a4f476422e9259d78765
neokee <kaiyuan.neo@gmail.com>
2013-02-28 22:12:50 -0800

    thursday night

---
+++ src/vm/pp.sh
@@ -0,0 +1,80 @@
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
+make build/tests/vm/page-parallel.result
+rm -f build/tests/vm/page-parallel.*
