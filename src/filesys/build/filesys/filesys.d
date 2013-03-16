29f749b35a26926422d6d8515f6b7fb10b3e1b72
neokee <kaiyuan.neo@gmail.com>
2013-03-16 00:38:10 -0700

    submitted

--- src/filesys/build/filesys/filesys.d
+++
@@ -1,9 +0,0 @@
-filesys/filesys.o: ../../filesys/filesys.c ../../filesys/filesys.h \
-  ../../lib/stdbool.h ../../filesys/off_t.h ../../lib/stdint.h \
-  ../../threads/synch.h ../../lib/kernel/list.h ../../lib/stddef.h \
-  ../../lib/debug.h ../../lib/stdio.h ../../lib/stdarg.h \
-  ../../lib/kernel/stdio.h ../../lib/string.h ../../filesys/cache.h \
-  ../../lib/kernel/hash.h ../../lib/kernel/list.h ../../devices/block.h \
-  ../../lib/inttypes.h ../../filesys/file.h ../../filesys/free-map.h \
-  ../../filesys/inode.h ../../filesys/directory.h ../../threads/thread.h \
-  ../../threads/synch.h ../../devices/block.h
--- src/filesys/build/lib/kernel/fixed-point.d
+++
@@ -1,2 +0,0 @@
-lib/kernel/fixed-point.o: ../../lib/kernel/fixed-point.c \
-  ../../lib/kernel/fixed-point.h ../../lib/stdint.h
